ed653c8a3f50831772208209f158573c3afd8ab4
hirayclay <hirayclay@gmail.com>
2025-03-15 14:22:08 +0800

    add: data to ui logic

--- app/src/main/java/com/onchain/test/MainActivity.kt
+++ app/src/main/java/com/onchain/test/MainActivity.kt
@@ -2,14 +2,23 @@ package com.onchain.test
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.onchain.test.databinding.ActivityMainBinding
+import com.onchain.test.ui.adapter.WalletBalanceAdapter
+import com.onchain.test.vm.MainViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private val walletBalanceAdapter = WalletBalanceAdapter()
     private val viewBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initUi()
+    }
+
+    private fun initUi() {
+        viewBinding.walletRecyclerView.adapter = walletBalanceAdapter
+
+        viewBinding.vm = viewModel
+        viewBinding.lifecycleOwner = this
+        //update balance list for all tokens
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.assetDataFlow.collect{
+                    walletBalanceAdapter.update(it)
+                }
+            }
         }
     }
 
